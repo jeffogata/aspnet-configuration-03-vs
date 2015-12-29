c8c567ae2413ed31743ebd628a245f4b4cf09e55
Jeff Ogata <jeffogata@outlook.com>
2015-12-29 09:18:13 -0600

    initial commit

---
+++ AspNetConfigurationVS/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace AspNetConfigurationVS
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Hosting;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.Configuration;
+
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName.ToLower()}.json", true);
+
+            _configuration = builder.Build();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var commonSetting = _configuration["commonSetting"];
+            var environmentSetting = _configuration["environmentSetting"];
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(
+                    $"<div>Setting = {commonSetting}</div>" +
+                    $"<div>Default Connection = {environmentSetting}</div>");
+            });
+        }
+    }
+}
